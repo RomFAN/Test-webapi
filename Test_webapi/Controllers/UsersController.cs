0c52b30891bc56c0ad8539f331575f19262f1252
r.kireyeu@godeltech.com <r.kireyeu@godeltech.com>
2019-03-07 12:14:50 +0300

    base classe added

---
+++ Test_webapi/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Test_webapi.Services;
+using Test_webapi.Entities;
+
+namespace Test_webapi.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
